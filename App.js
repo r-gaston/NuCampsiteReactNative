40ed53fc6cdb32d08eefc5928db31c75b2264ba7
r-gaston <rgaston001@columbus.rr.com>
2021-11-18 08:05:21 -0500

    Components Part 1

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Main from './components/MainComponent';
 
 export default function App() {
@@ -8,11 +6,3 @@ export default function App() {
       <Main />
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
