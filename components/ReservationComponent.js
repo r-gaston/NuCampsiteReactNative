7a979807ece1bea705fe024915b5845e1e84dd10
r-gaston <rgaston001@columbus.rr.com>
2021-11-29 20:24:13 -0500

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -44,8 +45,11 @@ class Reservation extends Component {
                 style: "cancel"                
               },
               {
-                text: "OK",                
-                onPress: () => {this.resetForm()} 
+                text: 'OK', 
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm();
+                    }
               },
             ],
             { cancelable: false }
@@ -62,8 +66,34 @@ class Reservation extends Component {
         });
     }
 
-       render() {
-        return (
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
+
+    render() {
+        return ( 
             <ScrollView>
                 <Animatable.View animation='zoomIn' duration={2000} delay={1000}>
                 <View style={styles.formRow}>
