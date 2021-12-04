3302b235b44bff3b3f1e954186fecaaa616e7ac6
r-gaston <rgaston001@columbus.rr.com>
2021-12-04 11:31:32 -0500

    Workshop 4

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -3,9 +3,12 @@ import { View, StyleSheet, ScrollView, Image } from 'react-native';
 import { Input, CheckBox, Button, Icon } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
 import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
+
 
 class LoginTab extends Component {
 
@@ -159,11 +162,37 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri});
+                this.processImage(capturedImage.uri);
+                MediaLibrary.saveToLibraryAsync(capturedImage.uri);
             }
         }
     }
 
+
+    getImageFromGallery = async () => {        
+        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
+            }
+        }
+    }
+
+    
+
+    processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync( imgUri, [{resize: { width: 400 }}], { format: ImageManipulator.SaveFormat.PNG });        
+        console.log(processedImage);
+        this.setState({ imageUrl: processedImage.uri});
+    }
+
+    
     handleRegister() {
         console.log(JSON.stringify(this.state));
         if (this.state.remember) {
@@ -190,6 +219,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
